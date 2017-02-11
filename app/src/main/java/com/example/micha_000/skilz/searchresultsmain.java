80a1d6dfb0eca41788ff2aa03ad29a2f61d175b8
James <jamie8@shaw.ca>
2017-02-11 03:01:29 -0800

    createdsearch file

---
+++ app/src/main/java/com/example/micha_000/skilz/searchresultsmain.java
@@ -0,0 +1,47 @@
+package com.example.micha_000.skilz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class searchresultsmain extends AppCompatActivity {
+
+    ListView list;
+    String[] web = {
+            "Erik",
+            "Pav",
+    };
+
+    Integer[] imageId = {
+            R.drawable.erik,
+            R.drawable.pav
+    };
+    String[] score = {
+            "45",
+            "66"
+    };
+    String[] cost = {
+            "5",
+            "7"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+
+        SearchResults adapter = new SearchResults(searchresultsmain.this,web,imageId,cost,score);
+        list = (ListView)findViewById(R.id.list);
+            list.setAdapter(adapter);
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                    Toast.makeText(searchresultsmain.this,"You Clicked at " +web[ + position],Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    }
+}
