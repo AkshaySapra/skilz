1a9cc32675375687aea64e10429323b9e645456e
AkshaySapra <michaelsapra@hotmail.com>
2017-02-11 02:56:26 -0800

    Main Menu

--- app/src/main/java/com/example/micha_000/skilz/Main.java
+++ app/src/main/java/com/example/micha_000/skilz/Main.java
@@ -1,13 +1,58 @@
 package com.example.micha_000.skilz;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main extends AppCompatActivity {
 
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView lv = (ListView)findViewById(R.id.listViewCountry);
+        ArrayList<String> arrayCountry = new ArrayList<>();
+        arrayCountry.addAll(Arrays.asList(getResources().getStringArray(R.array.array_skills)));
+
+        adapter = new ArrayAdapter<>(
+                Main.this,
+                android.R.layout.simple_list_item_1,
+                arrayCountry);
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
