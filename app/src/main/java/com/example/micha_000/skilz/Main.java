ba62a128737b89805dc3d21555f6a302c188c610
bronav95 <pambhorkar@yahoo.com>
2017-02-10 22:07:09 -0700

    thia is akshjayu

--- app/src/main/java/com/example/micha_000/skilz/Main.java
+++ app/src/main/java/com/example/micha_000/skilz/Main.java
@@ -8,6 +8,6 @@ public class Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);\//thisispra
+        setContentView(R.layout.activity_main);\//thisisapra
     }
 }
